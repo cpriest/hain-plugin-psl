377fa49cb1f8a265474e0cda19f646f07e12bd2e
Clint Priest <github-hain-psl@rxv.me>
2016-12-19 21:08:05 -0600

    Switched to using .gitignore for defs/*.json5 rather than PhpStorm

---
+++ stubs/JetBrainsStubs.js
@@ -0,0 +1,12 @@
+// /**
+//  * @callback RequireFunction
+//  * @returns {Promise|*}
+//  */
+//
+// /**
+//  * Loads a module.
+//  * See <a href="http://nodejs.org/api/modules.html">http://nodejs.org/api/modules.html</a> for more information.
+//  * @param {String} moduleName
+//  * @returns {RequireFunction|*}
+//  */
+// function require(moduleName) {}
